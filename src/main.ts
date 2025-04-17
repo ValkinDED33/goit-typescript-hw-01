6afa976053513b8c9f1e73ef9ebdfc19dfb14b4c
ValkinDED <sonyerik289@gmail.com>
2025-04-17 22:56:28 +0200

    goit-typescript-hw-01

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import "./style.css";
-// src/main.ts
 
-function add(num1: number, num2: number) {
-  return num1 + num2;
+const app = document.getElementById("app");
+
+if (app) {
+  app.innerHTML = `
+    <h1>Welcome to TypeScript + Vite</h1>
+    <div class="card">
+      <button id="counter">Count is 0</button>
+    </div>
+    <p class="read-the-docs">Click the button to increase the counter.</p>
+  `;
+
+  const button = document.getElementById("counter") as HTMLButtonElement;
+  let count = 0;
+
+  button.addEventListener("click", () => {
+    count++;
+    button.textContent = `Count is ${count}`;
+  });
 }
-
-console.log(add(1, 2));
